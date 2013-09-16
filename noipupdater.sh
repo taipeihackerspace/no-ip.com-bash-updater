3d0fc96dc73a4767f61fb2bc9968d071b78b9bd2
Gergely Imreh <imrehg@gmail.com>
2013-09-16 13:56:47 +0800

    update to latest version with checking DNS as well

--- noipupdater.sh
+++ noipupdater.sh
@@ -8,14 +8,15 @@ LOGFILE=logdir/noip.log
 STOREDIPFILE=configdir/current_ip
 USERAGENT="Simple Bash No-IP Updater/0.4 antoniocs@gmail.com"
 
-if [ ! -e $STOREDIPFILE ]; then 
+if [ ! -e $STOREDIPFILE ]; then
 	touch $STOREDIPFILE
 fi
 
 NEWIP=$(wget -O - http://icanhazip.com/ -o /dev/null)
+DNSIP=$(dig +short $HOST)
 STOREDIP=$(cat $STOREDIPFILE)
 
-if [ "$NEWIP" != "$STOREDIP" ]; then
+if [ "$NEWIP" != "$STOREDIP" ] || [ "$DNSIP" != "$NEWIP" ]; then
 	RESULT=$(wget -O "$LOGFILE" -q --user-agent="$USERAGENT" --no-check-certificate "https://$USERNAME:$PASSWORD@dynupdate.no-ip.com/nic/update?hostname=$HOST&myip=$NEWIP")
 
 	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] $RESULT"
