ed64d26270081aad0929af498d73f0adac9f39fd
Gergely Imreh <imrehg@gmail.com>
2013-09-23 09:15:27 +0800

    fix parameter typo that has broken the script and use faster external IP discovery service

--- noipupdater.sh
+++ noipupdater.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# What service to use to get current IP address
+MYIPSERVICE="icanhazip.com"
+
 # Get script's directory
 # http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -8,11 +11,11 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . "${DIR}/.credentials"
 
 # Get current IP and where the DNS settings point to
-NEWIP=$(curl --silent ifconfig.me)
-DNSIP=$(dig +short $HOSTNAME)
+NEWIP=$(curl --silent $MYIPSERVICE)
+DNSIP=$(dig +short $NOIPHOSTNAME)
 
 if [ "$DNSIP" != "$NEWIP" ]; then
-	RESULT=$(curl --silent "https://$NOIPUSERNAME:$NOIPPASSWORD@dynupdate.no-ip.com/nic/update?hostname=$NOIPHOST&myip=$NEWIP")
+	RESULT=$(curl --silent "https://$NOIPUSERNAME:$NOIPPASSWORD@dynupdate.no-ip.com/nic/update?hostname=$NOIPHOSTNAME&myip=$NEWIP")
 	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] $RESULT"
 else
 	LOGLINE="[$(date +"%Y-%m-%d %H:%M:%S")] No IP change"
